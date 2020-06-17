f3ab47782e95aa79dcf6cb98925ae3e486d6e898
vldsl <vladyslava.solovei@gmail.com>
2020-06-18 00:32:00 +0200

    add basic play logic

--- client.py
+++ client.py
@@ -1,21 +1,49 @@
 import socket
 
+
+def draw_board(content):
+    if len(content) != 9:
+        #  board content len error
+        return
+    print('Current board:\n')
+    print(f'|{content[0]}|{content[1]}|{content[2]}|\n')
+    print(f'|{content[3]}|{content[4]}|{content[5]}|\n')
+    print(f'|{content[6]}|{content[7]}|{content[8]}|\n')
+
+
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client_socket.connect(('localhost', 7777))
 
+
+#  print welcome messag from server
+print(client_socket.recv(1024).decode())
+#  print match info from server
+print(client_socket.recv(1024).decode())
+
+#  confirmation to server that the player is ready to start
+client_socket.send(b'ready')
+
 while True:
-    message = input('Please enter the message or quit to end game:')
-    data_out = message.encode()
+    board_content = client_socket.recv(16).decode()
+    print(board_content)
+    draw_board(board_content)
+    is_my_turn = client_socket.recv(5).decode().strip()
+    print(is_my_turn)
 
-    client_socket.sendall(data_out)
+    if is_my_turn == 'YES':
+        while True:
+            try:
+                move = int(input('Your turn! Enter a position (1-9)>>>'))
+                if move in range(1, 10):
+                    break
+            except ValueError as err:
+                #  error
+                print('Move value have wrong type')
 
-    if message.lower() == 'quit':
-        break
-
-    data_in = client_socket.recv(1024)
-    message = data_in.decode()
-    print(message)
+        client_socket.sendall(str(move).encode())
+    else:
+        print('Wait for the other player move...')
 
 
 client_socket.shutdown(socket.SHUT_RDWR)
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import time
+import random
 
 lock_matching = threading.Lock()
 
@@ -12,13 +13,34 @@ class Player:
         self.client_address = client_address
         self.is_waiting = is_waiting
 
+    def send_matching_info(self):
+        self.connection.sendall(
+            f'''Your opponent is {self.opponent.id}. Game is getting started!
+            \r\n'''.encode()
+        )
+
+
+class Game:
+    def __init__(self, player1, player2):
+        self.player1 = player1
+        self.player2 = player2
+        #  init empty board
+        self.board_content = list('0'*9)
+        self.turns = 0
+
+
+choices = ['X', 'O']
+
 
 def matching_player(new_player):
     lock_matching.acquire()
     try:
         for player in players:
             if player.is_waiting and player.id != new_player.id:
-                new_player.match = player
+                new_player.opponent = player
+                player.opponent = new_player
+                new_player.sign = random.choice(choices)
+                player.sign = choices[0] if new_player.sign == choices[1] else choices[1]
                 player.is_waiting = new_player.is_waiting = False
                 return player
     finally:
@@ -27,7 +49,9 @@ def matching_player(new_player):
 
 
 def handle_client(player):
-    connection.sendall(b'Welcome to Tic Tac Toe\r\n')
+    player.connection.send(
+        b'''Welcome to Tic Tac Toe!\n
+        Please wait for another player to join the game ...\r\n''')
 
     while True:
         if player.is_waiting:
@@ -36,17 +60,51 @@ def handle_client(player):
             if not opponent:
                 time.sleep(1)
             else:
-                message = 'Your opponent is {}. Game is getting started!\r\n'
-                player.connection.sendall(message.format(opponent.id).encode())
-                opponent.connection.sendall(message.format(player.id).encode())
-        data_in = connection.recv(1024)
-        message = data_in.decode()
+                new_game = Game(player, opponent)
+                handle_game(new_game)
 
-        if message.lower() == 'quit':
-            break
+                return
+        else:
+            #  if the player has already been matched end the loop
+            return
 
-        print(f'Message from client: {message}')
-        connection.sendall(data_in)
+
+def handle_game(game):
+    game.player1.send_matching_info()
+    game.player2.send_matching_info()
+
+    player1_msg = game.player1.connection.recv(1024).decode().lower()
+    player2_msg = game.player2.connection.recv(1024).decode().lower()
+
+    if player1_msg == player2_msg == 'ready':
+        print('New game started')
+    else:
+        print('Error occured')
+
+    while True:
+        try:
+            game.player1.connection.sendall(''.join(game.board_content).encode())
+            game.player2.connection.sendall(''.join(game.board_content).encode())
+
+            game.player1.connection.sendall(b'YES\r\n')
+            game.player2.connection.sendall(b'NO\r\n')
+
+            move = int(game.player1.connection.recv(4).decode())
+            game.board_content[move-1] = game.player1.sign
+
+            game.player1.connection.sendall(''.join(game.board_content).encode())
+            game.player2.connection.sendall(''.join(game.board_content).encode())
+
+            game.player1.connection.sendall(b'NO\r\n')
+            game.player2.connection.sendall(b'YES\r\n')
+
+            move = int(game.player2.connection.recv(4).decode())
+            print(move)
+            game.board_content[move-1] = game.player2.sign
+
+        except ValueError as err:
+            #  send error message
+            return
 
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -58,7 +116,7 @@ players = []
 
 while True:
     connection, client_address = server_socket.accept()
-    new_player = Player(100 + len(players), connection, client_address, True)
+    new_player = Player(1 + len(players), connection, client_address, True)
     players.append(new_player)
     threading.Thread(target=handle_client, args=(new_player,)).start()
 
